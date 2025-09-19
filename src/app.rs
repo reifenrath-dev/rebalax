034d0efdc923fd664bc8075903357810d26c6b5b
René Reifenrath <rene@reifenrath.dev>
2025-09-19 18:37:40 +0200

    improve strategy picker & menu (#19)
    * implement icon switching for title bar menu icon
    
    * refactor strategy switcher
    
    * leptosfmt
    
    * bump dependencies
    
    * set version 0.2.6

--- src/app.rs
+++ src/app.rs
@@ -13,30 +13,7 @@ pub fn App() -> impl IntoView {
     view! {
         <I18nContextProvider>
             <Router>
-                <div id="titlebar-container">
-                    <div id="titlebar-spacer"></div>
-                    <div class="titlebar">
-                        <a class="titlebar-button" href="/">
-                            <img height=24 width=24 src="public/32x32.png" draggable="false" />
-                        </a>
-                        <a class="titlebar-button" href="/">
-                            <span class="titlebar-title">Rebalax</span>
-                        </a>
-                        <a
-                            class="titlebar-button"
-                            id="titlebar-menu"
-                            href=move || {
-                                if use_location().pathname.get().contains("menu") {
-                                    "/"
-                                } else {
-                                    "/menu"
-                                }
-                            }
-                        >
-                            <MenuIcon />
-                        </a>
-                    </div>
-                </div>
+                <TitleBar />
                 <Routes fallback=|| "Not found">
                     <I18nRoute<Locale, _, _> view=|| view! { <Outlet /> }>
                         <Route path=path!("/") view=Rebalancer />
@@ -45,5 +22,44 @@ pub fn App() -> impl IntoView {
                 </Routes>
             </Router>
         </I18nContextProvider>
+    }
+}
+
+#[component]
+pub fn TitleBar() -> impl IntoView {
+    view! {
+        <div id="titlebar-container">
+            <div id="titlebar-spacer" />
+            <div class="titlebar">
+                <a class="titlebar-button" href="/">
+                    <img height=24 width=24 src="public/32x32.png" draggable="false" />
+                </a>
+                <a class="titlebar-button" href="/">
+                    <span class="titlebar-title">Rebalax</span>
+                </a>
+                <SwitchMenuButton />
+            </div>
+        </div>
+    }
+}
+
+#[component]
+pub fn SwitchMenuButton() -> impl IntoView {
+    move || {
+        if use_location().pathname.get().contains("menu") {
+            view! {
+                <a class="titlebar-button" id="titlebar-menu" href="/">
+                    <CloseIcon />
+                </a>
+            }
+            .into_any()
+        } else {
+            view! {
+                <a class="titlebar-button" id="titlebar-menu" href="/menu">
+                    <MenuIcon />
+                </a>
+            }
+            .into_any()
+        }
     }
 }
