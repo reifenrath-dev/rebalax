46c9f016fbac52ff90b0131a3761886bcaf96d72
reifenrath-dev <rene@reifenrath.dev>
2025-09-24 16:46:45 +0200

    add privacy policy link to the menu

--- src/components.rs
+++ src/components.rs
@@ -174,6 +174,30 @@ pub fn CloseIcon() -> impl IntoView {
 }
 
 #[component]
+pub fn PrivacyIcon() -> impl IntoView {
+    view! {
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-hat-glasses-icon lucide-hat-glasses"
+        >
+            <path d="M14 18a2 2 0 0 0-4 0" />
+            <path d="m19 11-2.11-6.657a2 2 0 0 0-2.752-1.148l-1.276.61A2 2 0 0 1 12 4H8.5a2 2 0 0 0-1.925 1.456L5 11" />
+            <path d="M2 11h20" />
+            <circle cx="17" cy="18" r="3" />
+            <circle cx="7" cy="18" r="3" />
+        </svg>
+    }
+}
+
+#[component]
 pub fn DiffString(diff: Decimal, has_braces: bool) -> impl IntoView {
     if diff.is_zero() {
         view! { <span class="zero">{"".to_string()}</span> }
